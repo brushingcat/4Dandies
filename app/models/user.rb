59e3af858f671324f8b1d15ce6bb8fadadf92ea4
Ricardo Moreira <rddm@4dandies.com>
2016-08-11 17:21:49 +0100

    Subscrição por email feita

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  #devise :database_authenticatable, :registerable,
+  #       :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :validatable
+
+  protected
+  def password_required?
+    false
+  end
+end
