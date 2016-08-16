d1c6b4b60eedec3ba71cbc6f26cfb85d1cacad47
Ricardo Moreira <rddm@4dandies.com>
2016-08-16 12:31:13 +0100

    Resolved issues #16 and #17

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,53 +1,30 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
-    else
-      session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    generic_callback('Facebook')
   end
 
 
   def google_oauth2
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      sign_in_and_redirect @user, :event => :authentication
-    else
-      session["devise.google_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    generic_callback('Google')
   end
 
   def twitter
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-
-    if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Twitter") if is_navigational_format?
-    else
-      session["devise.twitter_data"] = request.env["omniauth.auth"]
-      redirect_to new_user_registration_url
-    end
+    generic_callback('Twitter')
   end
 
   def instagram
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.from_omniauth(request.env["omniauth.auth"])
+    generic_callback('Instagram')
+  end
 
+  def generic_callback( provider )
+    @user=User.from_omniauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Instagram") if is_navigational_format?
+      #sign_in_and_redirect @user, :event => :authentication #this will throw
+      # if @user is not activated
+      redirect_to root_path
+      set_flash_message(:notice, :success, :kind => "#{provider}") if is_navigational_format?
     else
-      session["devise.instagram_data"] = request.env["omniauth.auth"]
+      #session["devise.data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "users/registrations",
-                                      :omniauth_callbacks => "users/omniauth_callbacks"  }
+                                      :omniauth_callbacks =>
+                                          "users/omniauth_callbacks"  },
+             :skip => :sessions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
