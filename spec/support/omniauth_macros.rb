e0e1471dfe14394b7a33465242a347447edd5b9f
Antonio <apinheiro@4dandies.com>
2016-08-19 12:20:12 +0100

    Integrations Tests for Login with Omniauth

---
+++ spec/support/omniauth_macros.rb
@@ -0,0 +1,25 @@
+# in spec/support/omniauth_macros.rb
+
+module OmniauthMacros
+  def createAutoHash (provider = "facebook")
+    OmniAuth::AuthHash.new({
+         provider: provider,
+         uid: '123545',
+         info: {
+             first_name: "Gaius",
+             last_name:  "Baltar",
+             email:      "test@example.com",
+             image_url:  "image.com"
+         },
+         credentials: {
+             token: "123456",
+             expires_at: Time.now + 1.week
+         },
+         extra: {
+             raw_info: {
+                 gender: 'male'
+             }
+         }
+     })
+  end
+end
