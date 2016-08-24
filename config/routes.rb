769e93420a0cca7428f053b2233bef797f11368c
Antonio <apinheiro@4dandies.com>
2016-08-24 14:19:43 +0100

    Criacao da pagina do Blog

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
              :skip => :sessions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'application#index'
+  #root to: 'application#index'
+  get  'about'    => 'clean_blogs#about'
+  get  'post'     => 'clean_blogs#post'
+  get  'contact'  => 'clean_blogs#contact'
+  root 'clean_blogs#index'
 end
