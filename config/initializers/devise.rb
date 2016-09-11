80acebcec643f154c98cd80b8bee3124af5deb52
Ricardo Moreira <rddm@4dandies.com>
2016-09-11 22:22:32 +0100

    Getting larger pictures using omniauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -250,14 +250,19 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET']
+  config.omniauth :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_APP_SECRET'],
+                  :display => 'popup',
+                  :image_size => {width: 100, height: 100}
 
-  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET']
+  config.omniauth :google_oauth2, ENV['GOOGLE_APP_ID'], ENV['GOOGLE_APP_SECRET'],
+                  :image_aspect_ratio => "square",
+                  :image_size => 100
 
   config.omniauth :instagram, ENV['INSTAGRAM_APP_ID'],
                   ENV['INSTAGRAM_APP_SECRET']
 
-  config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET']
+  config.omniauth :twitter, ENV['TWITTER_APP_ID'], ENV['TWITTER_APP_SECRET'],
+                  :image_size => 'original'
 
 
   # ==> Warden configuration
