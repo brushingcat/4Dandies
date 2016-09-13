3d2e58d0bbacbc677df2dfbdd4f771b7f4f417d4
Ricardo Moreira <rddm@4dandies.com>
2016-09-13 03:08:30 +0100

    Solved issue when invalid data at subscribe

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,52 +1,75 @@
 class Users::RegistrationsController < Devise::RegistrationsController
- before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
 # before_action :configure_account_update_params, only: [:update]
 
-  # GET /resource/sign_up
+# GET /resource/sign_up
   def new
     @users = User.where.not(:image_url => nil).shuffle.take(200)
-     super
+    super
   end
 
-  # POST /resource
-  # def create
-  #   super
-  # end
+# POST /resource
+# def create
+#   super
+# end
 
-  # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def create
+    build_resource(sign_up_params)
 
-  # PUT /resource
-  # def update
-  #   super
-  # end
+    resource.save
+    yield resource if block_given?
+    if resource.persisted?
+      if resource.active_for_authentication?
+        set_flash_message! :notice, :signed_up
+        sign_up(resource_name, resource)
+        respond_with resource, location: after_sign_up_path_for(resource)
+      else
+        set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        respond_with resource, location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      resource.errors.full_messages.each { |x| flash[x] = x } # Rails 4 simple way
+      redirect_to new_user_registration_path
+    end
+  end
 
-  # DELETE /resource
-  # def destroy
-  #   super
-  # end
+# GET /resource/edit
+# def edit
+#   super
+# end
 
-  # GET /resource/cancel
-  # Forces the session data which is usually expired after sign
-  # in to be expired now. This is useful if the user wants to
-  # cancel oauth signing in/up in the middle of the process,
-  # removing all OAuth session data.
-  # def cancel
-  #   super
-  # end
+# PUT /resource
+# def update
+#   super
+# end
 
-   protected
+# DELETE /resource
+# def destroy
+#   super
+# end
 
-   def sign_up(resource_name, resource)
-     true
-   end
+# GET /resource/cancel
+# Forces the session data which is usually expired after sign
+# in to be expired now. This is useful if the user wants to
+# cancel oauth signing in/up in the middle of the process,
+# removing all OAuth session data.
+# def cancel
+#   super
+# end
 
-  # If you have extra params to permit, append them to the sanitizer.
-   def configure_sign_up_params
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-   end
+  protected
+
+  def sign_up(resource_name, resource)
+    true
+  end
+
+# If you have extra params to permit, append them to the sanitizer.
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
