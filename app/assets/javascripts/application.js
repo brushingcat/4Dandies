16e31b04d82b463a5f1ca3ec503def9dc7e48b31
Ricardo Moreira <rddm@4dandies.com>
2016-09-28 19:10:06 +0100

    Modal fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -36,7 +36,7 @@ $(document).on('turbolinks:load', function () {
     });
 
     //scroll animation
-    $('a[href*="#"]:not([href="#"])').click(function () {
+    $('a[href*="#"]:not([href="#"]):not([href="#myModal"])').click(function () {
         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
             var target = $(this.hash);
             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
